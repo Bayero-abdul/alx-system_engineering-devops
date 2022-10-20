6c39bb156b22541f2759000b8f7f9386df721a94
Bayero-abdul <bayeroa65@gmail.com>
2022-10-20 09:45:49 +0000

    Update regex

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-text = ARGV[0].scan(/\[from:([\d\w]+)\] \[to:(.?\d+)\] \[flags:(.+-1)\]/)[0]
+text = ARGV[0].scan(/\[from:([\d\w+]*)\] \[to:(.?\d+)\] \[flags:(.+-1)\]/)[0]
 puts "#{text[0]},#{text[1]},#{text[2]}"
