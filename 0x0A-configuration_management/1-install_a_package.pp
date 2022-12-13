9114d987ba86aec9d8b179aeb3bd4bf694fa1fe5
Bayero-abdul <bayeroa65@gmail.com>
2022-12-13 09:40:55 +0000

    Update code

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # install flask from pip
 exec {'install-flask':
-  command => 'usr/bin/pip3 install flask==2.1.0',
-  unless  => 'pip3 freeze | grep flask==2.1.0',
+  command => '/bin/pip3 install flask==2.1.0',
   path    => '.'
 }
