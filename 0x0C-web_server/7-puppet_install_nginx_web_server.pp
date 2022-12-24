eacddc3c35bf52e1dd01cd8f077a964987c7756e
Bayero-abdul <bayeroa65@gmail.com>
2022-12-24 09:20:31 +0000

    Test code

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,17 +1,13 @@
 # configuration for nginx server
 
-exec {'/usr/bin/env apt-get update':}
+exec {'/usr/bin/env apt-get -y update':}
 
 exec {'/usr/bin/env apt-get -y install nginx':}
 
 exec {'/usr/bin/env echo "Hello World!" > /var/www/html/index.html':}
 
-$location='\n\tlocation /redirect_me {\n\trewrite ^/redirect_me(.*)$ https://www.grymoire.com/Unix/Sed.html permanent;\n\t}'
+exec {'/usr/bin/env sed "/server_name _;/ a\ \\n\\tlocation /redirect_me {\\n\\trewrite ^/redirect_me(.*)$ https://www.grymoire.com/Unix/Sed.html permanent;\\n\\t}" -i /etc/nginx/sites-enabled/default':}
 
-exec { "/usr/bin/env sed '/server_name _;/ a\ ${location}' -i /etc/nginx/sites-enabled/default":}
+exec {'/usr/sbin/env nginx -t':}
 
-exec {'/usr/bin/env sed -i "/server_name _;/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-enabled/default':}
-
-exec {'/usr/bin/env echo "Ceci n\'est pas une page" > /var/www/html/custom_404.html':}
-
-exec {'/usr/bin/env service nginx restart':}
+exec {'/usr/sbin/env service nginx restart':}
