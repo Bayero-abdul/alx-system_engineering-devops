28089ef3616cddf16bb3e44c4704882109ebf509
Bayero-abdul <bayeroa65@gmail.com>
2023-01-17 17:34:21 +0000

    Refactor code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,66 +1,28 @@
 #!/usr/bin/python3
-
-"""display on the standard output the employee TODO list progress.
+"""using this REST API,
+for a given employee ID,
+return information about his/her TODO list progress.
 
 """
+
 
 if __name__ == '__main__':
 
     import requests
     import sys
 
-    try:
-        id = int(sys.argv[1])
-    except TypeError:
-        print("argument must be int")
+    id = int(sys.argv[1])
+    API = 'https://jsonplaceholder.typicode.com'
+    json_todos = requests.get(f'{API}/todos').json()
+    u_name = requests.get(f'{API}/users/{id}').json().get('name')
 
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-    json_todos = todos.json()
-    users = requests.get('https://jsonplaceholder.typicode.com/users')
-    json_users = users.json()
+    tasks = list(filter(lambda u_id: u_id.get('userId') == id,
+                        json_todos))
+    tasks_done = list(filter(lambda u_id: u_id.get('completed'),
+                             tasks))
 
-    name = get_name(id, json_users)
-    cnt_done, cnt_tasks, titles = get_employee_details(id, json_todos)
+    print('Employee {} is done with tasks({}/{}):'.format(
+            u_name, len(tasks_done), len(tasks)))
 
-    title_string = ""
-    for i in range(len(titles)):
-        if i == len(titles) - 1:
-            title_string += titles[i]
-        else:
-            title_string += titles[i] + '\n\t'
-
-    output = f'Employee {name} is done with tasks({cnt_done}/{cnt_tasks}):\
-               \n\t{title_string}'
-    print(output)
-
-
-def get_name(id, json_users):
-    """get name of id"""
-
-    for user in json_users:
-        if user.get('id') == id:
-            return user.get('name')
-    return ""
-
-
-def get_employee_details(id, json_todos):
-    """return employee details"""
-
-    cnt_done = 0
-    cnt_tasks = 0
-    titles = []
-    state = 0
-
-    for user in json_todos:
-        if user.get('userId') == id and user.get('completed'):
-            state = 1
-            cnt_done += 1
-            cnt_tasks += 1
-            titles.append(user.get('title'))
-        elif user.get('userId') == id:
-            state = 1
-            cnt_tasks += 1
-        elif state == 1:
-            break
-
-    return (cnt_done, cnt_tasks, titles)
+    for x in tasks_done:
+        print('\t {}'.format(x.get('title')))
