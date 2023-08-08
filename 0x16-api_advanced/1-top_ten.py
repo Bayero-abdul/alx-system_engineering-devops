1aebeaad5f4b1fbd5d404240c51d230f3436d4a6
Bayero-abdul <bayeroa65@gmail.com>
2023-08-08 14:40:21 +0100

    Update code

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,6 +12,10 @@ def top_ten(subreddit):
     Returns the titles of
     the first 10 hot posts listed for a given subreddit
     """
+    if (subreddit is None):
+        print(None)
+        return
+
     headers = {
         "User-Agent": "Github/1.0 by Bayero-abdul (Contact: bayeroa65@gmail.com)"
     }
@@ -22,7 +26,14 @@ def top_ten(subreddit):
 
     if response.status_code == 200:
         data = response.json().get('data')
+        if data is None:
+            print(None)
+            return
         top = data.get('children')[:10]
+        if top is None:
+            print(None)
+            return
+
         for i in range(10):
             print(top[i].get('data').get('title'))
     else:
