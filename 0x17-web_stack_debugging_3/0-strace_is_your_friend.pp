12856259f10d487309d82156bbae1c50b8e0a183
Bayero-abdul <bayeroa65@gmail.com>
2023-08-10 00:08:17 +0100

    Fix spelling errors on server

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Puppet script that fixes a bug in a WordPress website
+
+exec { 'replace_line':
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+}
