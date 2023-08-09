35d86c1114836b5f1ef4f4c20065064788fa73d8
Bayero-abdul <bayeroa65@gmail.com>
2023-08-09 22:08:07 +0100

    Fix line too long

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,14 +9,15 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Returns the number of subscribers (not active users, total subscribers) for a given subreddit
+    Returns the number of subscribers (not active users, total subscribers)
+    for a given subreddit
     """
     if (subreddit is None):
         return 0
 
-    headers = {
-        "User-Agent": "Github/1.0 by Bayero-abdul (Contact: bayeroa65@gmail.com)"
-    }
+    v = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101"
+    v += "Firefox/15.0.1"
+    headers = {'User-agent': v}
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(
