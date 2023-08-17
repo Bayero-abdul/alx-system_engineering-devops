8e225480481bac0ecaa919154eafceaf73bb278b
Bayero-abdul <bayeroa65@gmail.com>
2023-08-17 23:26:07 +0000

    Add tasks

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,7 @@
+# Puppet scripts that increases the limit of opened files
+
+exec { 'increase_os_worker_files':
+  provider => shell,
+  command  => 'sudo sed -i "/holberton hard/s/5/65536/" /etc/security/limits.conf',
+  path     => '/usr/bin/:/usr/local/bin/:/bin/'
+}
